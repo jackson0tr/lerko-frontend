a8ea5c964d245edff865dff0c77d6aab39eba151
jackson0tr <mahmoudmohamedm82@gmail.com>
2024-09-14 14:32:06 +0200

    forgot

--- app/components/Auth/ForgotPassword.tsx
+++ app/components/Auth/ForgotPassword.tsx
@@ -1,25 +1,25 @@
-'use client';
-import React, { FC, useEffect } from 'react';
-import { style } from '@/app/style/style';
-import { useForgetPasswordMutation } from '@/redux/features/auth/authApi';
-import { useFormik } from 'formik';
-import { toast } from 'react-hot-toast';
+'use client'
+import React, { FC, useEffect, useState } from "react";
+import { useFormik } from "formik";
 import * as Yup from "yup";
+import { style } from "../../style/style";
+import { toast } from "react-hot-toast";
+import { useForgetPasswordMutation } from '@/redux/features/auth/authApi';
 
 type Props = {
-    openPass: boolean;
-    setOpenPass: (openPass: boolean) => void;
-    setRoute?: (route: string) => void;
+    setRoute: (route: string) => void;
     setOpen: (open: boolean) => void;
-    open?: boolean;
-    // refetch?: any;
+    refetch?: any;
 }
 
-const ForgetPassword: FC<Props> = ({ setRoute, setOpen, openPass, setOpenPass }) => {
 
-    const schema = Yup.object().shape({
-        email: Yup.string().email('بريد إلكتروني خاطئ').required("رجاءا أدخل بريدك الإلكتروني"),
-    });
+
+const schema = Yup.object().shape({
+    email: Yup.string().email("الايميل ليس موجود").required("يرجي ادخال البريد الالكتروني"),
+});
+
+const ForgotPassword: FC<Props> = ({ setRoute, setOpen, refetch }) => {
+    const [openPass, setOpenPass] = useState(false);
 
     const [forgetPassword, { data, isSuccess, error }] = useForgetPasswordMutation();
 
@@ -30,7 +30,6 @@ const ForgetPassword: FC<Props> = ({ setRoute, setOpen, openPass, setOpenPass })
             await forgetPassword({ email });
         }
     });
-
 
     useEffect(() => {
         if (isSuccess) {
@@ -46,43 +45,52 @@ const ForgetPassword: FC<Props> = ({ setRoute, setOpen, openPass, setOpenPass })
         }
     }, [isSuccess, error]);
 
-    const { errors, touched, values, handleChange, handleSubmit } = formik;
 
+
+    const { errors, touched, values, handleChange, handleSubmit } = formik;
     return (
         <>
-            <div className='container w-[400px] h-[400px] mx-auto px-5 py-5 items-center justify-center flex'>
-                <div className="bg-[#fff]  shadow-md w-full rounded">
-                    <h1 className={`${style.title}`}>
-                        ادخل البريد الالكتروني
-                    </h1>
-                    <form className='p-4' onSubmit={handleSubmit}>
-                        <label
-                            htmlFor="email"
-                            className={`${style.label}`}>
-                            أدخل بريدك الإلكتروني
-                        </label>
-                        <input
-                            type="email"
-                            name="email"
-                            value={values.email}
-                            onChange={handleChange}
-                            id="email"
-                            placeholder="أدخل بريدك الإلكتروني"
-                            className={`${errors.email && touched.email && "border-red-500"} ${style.input}`}
-                        />
-                        <br />
-                        <span onClick={() => setOpenPass(false)} className="text-[#2190ff]  cursor-pointer">
-                            الرجوع
-                        </span>
-                        <div className="w-full mt-5">
-                            <input type="submit" value="ارسال" className={`${style.button}`} />
-                        </div>
-                    </form>
+            <div className={`w-full ${openPass ? "hidden" : "block"}`}>
+                <h1 className={`${style.title}`}>
+                    ادخل البريد الالكتروني
+                </h1>
+                <form className="p-4" onSubmit={handleSubmit}>
+
+                    <label
+                        htmlFor="email"
+                        className={`${style.label}`}>
+                        أدخل بريدك الإلكتروني
+                    </label>
+
+                    <input
+                        type="email"
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        id="email"
+                        placeholder="أدخل بريدك الإلكتروني"
+                        className={`${errors.email && touched.email && "border-red-500"} ${style.input}`}
+                    />
+
+                    {errors.email && touched.email && (
+                        <span className="text-red-500 pt-2 block">{errors.email}</span>
+                    )}
                     <br />
-                </div>
+                    <br />
+                    <input
+                        type="submit"
+                        value="ارسال"
+                        className={`${style.button} mt-3`}
+                    />
+                    <br />
+                    <span onClick={() => setOpenPass(false)} className="text-[#2190ff]  cursor-pointer">
+                        الرجوع
+                    </span>
+                </form>
+                <br />
             </div>
         </>
     )
 }
 
-export default ForgetPassword
+export default ForgotPassword;
